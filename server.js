d11e2d8ae01875e2eb88793b53c013008b76e49c
Rahulrs7 <Rahulsagar17000@gmail.com>
2025-03-19 14:40:50 +0530

    7

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ import cors from 'cors'
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoute.js"
 import foodRouter from "./routes/foodRoute.js"
-// import cartRouter from "./routes/cartRoute.js"
-// import orderRouter from "./routes/orderRoute.js"
+
 
 // app config
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 
 // middlewares
-app.use(cors);
-
+app.use(express.json())
+app.use(cors())
 
 // db connection
 connectDB()
@@ -23,8 +22,6 @@ connectDB()
 app.use("/api/user", userRouter)
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
-// app.use("/api/cart", cartRouter)
-// app.use("/api/order",orderRouter)
 
 app.get("/", (req, res) => {
     res.send("API Working")
