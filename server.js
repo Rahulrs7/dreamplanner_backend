f659ba432f6e82c3135861081aa98ca8e8eda91d
Rahulrs7 <Rahulsagar17000@gmail.com>
2025-03-19 13:24:53 +0530

    2

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({ 
+  origin: "https://dreamplanner-frontend.vercel.app", 
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
+
 
 // db connection
 connectDB()
